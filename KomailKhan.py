e3cdac0a77c5d64b1f5c639d2955af1264e12316
unknown1234565 <72522994+unknown1234565@users.noreply.github.com>
2020-10-14 01:31:13 +0200

    Update KomailKhan.py

--- KomailKhan.py
+++ KomailKhan.py
@@ -130,8 +130,8 @@ print  """\033[1;91m
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "komail"
-CorrectPassword = "khan"
+CorrectUsername = "TINO
+CorrectPassword = "TINO"
 
 loop = 'true'
 while (loop == 'true'):
